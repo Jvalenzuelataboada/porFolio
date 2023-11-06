21b2959d379864298629866b6d1fc57f0cb8bee4
CarritodeFundamentos <jvalenzuelataboada@gmail.com>
2023-11-05 19:59:09 -0500

    portfolio 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ return(
                     <img className='w-[80%] max-md:flex max-md:items-center max-md:justify-center' src={('./public/robot1.png')} alt="" />
                 </div>
                 <div className="">
-                    <h2 className="text-xl"><span className="text-2xl">Hi!</span> I'm a Front End Developer.</h2>
+                    <h2 className="text-xl"><span className="text-2xl">Hi!</span> Soy a Front End Developer.</h2>
                     <h3 className="text-xl">My name is <span className="text-2xl">Juan Carlos Valenzuela</span></h3>
                     <h6>Lorem ipsum dolor, sit amet consectetur</h6>
                 </div>
